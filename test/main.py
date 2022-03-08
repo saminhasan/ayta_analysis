210ab6bd049464eeeb6b134a27fb43b0277d6b8b
Saminhasan5220 <saminhasan@iut-dhaka@edu.com>
2022-03-09 00:51:59 +0600

    added mdns based ip address finder

--- test/main.py
+++ test/main.py
@@ -1,12 +1,16 @@
 import json
+import socket
 import requests
 from demo import*
+
 import pandas as pd
 import tkinter as tk
 
 
 class Application(tk.Frame):
 	def __init__(self, master=None):
+		self.DEVICE_IP = self.find_device()
+
 		super().__init__(master)
 		self.master = master
 		self.master.geometry("320x240")
@@ -14,6 +18,21 @@ class Application(tk.Frame):
 		self.create_widgets()
 		self.recording = False
 
+		
+	def find_device(self):
+		IP = ''
+		while True:
+			try:
+				IP = socket.gethostbyname('raspberrypi.local')
+				print(f' Device IP  = {IP}')
+				if IP:
+					break
+			except Exception as e:
+				print('Error! Code : {c}, Message :  {m}'.format(c = type(e).__name__, m = str(e)))
+				#print(type(e).__name__)
+				#if 
+				print("Device Not Found")
+		return IP
 	def create_widgets(self):
 		self.start = tk.Button(self)
 		self.start["text"] = "Start"
@@ -27,7 +46,7 @@ class Application(tk.Frame):
 		
 		# TextBox Creation
 		self.inputtxt = tk.Text(self, height = 1, width = 24)
-		self.inputtxt.insert(tk.END, "192.168.1.109")
+		self.inputtxt.insert(tk.END, self.DEVICE_IP)
 		self.inputtxt.pack(fill = "both", expand = True, pady=5)
 
 
@@ -56,6 +75,8 @@ class Application(tk.Frame):
 		print(data.status_code)
 		data = json.loads(data.text)
 		df = pd.DataFrame(data)
+		df = df.fillna(0)
+		df = df.fillna(0)
 		#print(df.columns)
 		#print(df.head())
 		if self.recording and len(df) > 10:
@@ -64,21 +85,23 @@ class Application(tk.Frame):
 
 	@staticmethod
 	def foo(df):
-
 		show_data(df)
 		
 		
 def main():
+	
+
+	
+	root = tk.Tk()
+	app = Application(master=root)
+	app.mainloop()
+	'''
 	try:
-		root = tk.Tk()
-		app = Application(master=root)
-		app.mainloop()
-		'''
 		while True:
 			pass
-		'''
 	except KeyboardInterrupt:
 		print("KeyboardInterrupt")
+	'''
 if __name__ == "__main__":
 	main()
 
